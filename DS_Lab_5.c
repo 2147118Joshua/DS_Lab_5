613148766e456f1554c07bd8f1ea7e7ebf51610b
Joshua Stalin <95157467+2147118Joshua@users.noreply.github.com>
2022-04-07 09:08:13 +0530

    Update DS_Lab_5.c

--- DS_Lab_5.c
+++ DS_Lab_5.c
@@ -1,47 +1,158 @@
 #include<stdio.h>
-#include<time.h>
-#include <stdlib.h>
+#include<stdlib.h>
+#define MAX 8
+
+
+void insertionsort();
+void selectionsort();
+void bubblesort();
+void display(int *);
+void swap(int *, int *);
+
+int count = 0;
 int main()
 {
-// generating 100 randam elements in range 1 to 200
-srand(time(0));
-int i,j, data, x,k;
-float avg_success;
-float av;
-int s_count= 0;
-int ss_count= 0;
-int a[100];
-for(i = 0; i<100; i++)
-{
-data= (srand() % ((200  - 1)+1));
-        printf(" %d ", data);
-  a[j] = data;
-  }
-  
-  
-    x= (srand() % ((200  - 1)+1));
-    printf("\n\n element to be searched is %d", x);
-    for(k=0;k<100;++k)
+    int arr[MAX]={83,25,243,2,24,5,42,34};
+    int i, j, key;
+    int choice,noofelements;
+    while(1)
     {
-s_count++;
-        if(a[k]==x)
-        ss_count++;
-            break;
+        printf("\n1. insertion sort\n");
+        printf("\n2. Seletion sort");
+        printf("\n3. Quick Sort");
+        printf("\n4. Exit\n");
+        printf("\nEnter your choice: ");
+        scanf("%d", &choice);
+    switch(choice)
+    {
+        int i;
+        case 1:    insertionsort();
+                break;
+        case 2: selectionsort();
+                break;
+        case 3: bubblesort();
+                break;
+        case 4: exit(1);
+                break;
+        default:printf("\n Invalid input!!!\n");
+                break;
+    }
+    }
+    return 0;
 }
-    if(k<100)
-        printf("\n\nElement found at index %d",k);
-    else
-        printf("\n\nElement not found");
+
+
+
+    
+void insertionsort()
+{
+    count=0;
+    int arr[MAX]={83,25,243,2,24,5,42,34};
+    int i, j, key;
+    
+    printf("\nArray before sorting:\n");
+    for( i = 0; i < MAX; i++)
+    {
+        printf("%d\t", arr[i]);
+    }
+    printf("\n\n\n");
+    
+    for(i=0; i< MAX; i++)
+    {
+        key = arr[i];
+        j=i-1;
+        count++;
+        while(j>=0 && arr[j] > key)
+        {
+            arr[j+1] = arr[j];
+            j--;
+            count++;
+        }
+        arr[j+1] = key;
+    }
+    
+display(arr);
+    printf("\n Time complexity : %d\n", count);
+    
+}
+
+
   
+void selectionsort()
+{
+    count=0;
+    int arr[MAX]={83,25,243,2,24,5,42,34};
+    int i, j, min_idx;
+    
+    printf("\nArray before sorting:\n");
+    for( i = 0; i < MAX; i++)
+    {
+        printf("%d\t", arr[i]);
+    }
+    printf("\n\n\n");
+  
+    // One by one move boundary of unsorted subarray
+    for (i = 0; i < MAX-1; i++)
+    {
+        // Find the minimum element in unsorted array
+        min_idx = i;
+        count++;
+        for (j = i+1; j < MAX; j++)
+        count++;
+          if (arr[j] < arr[min_idx])
+            min_idx = j;
+  
+        // Swap the found minimum element with the first element
+        swap(&arr[min_idx], &arr[i]);
+    }
+    display(arr);
+    printf("\n Time complexity : %d\n", count);
 
-printf("\n\n\n number of searches completed %d", s_count);
-printf("\n\nThe number of successful searches %d" , ss_count);
-avg_success = (ss_count / s_count) *100;
-av= (ss_count / s_count) ;
+}
 
-printf("\n\n\n The average number of tests per search %d", av);
 
-  printf("\n\nThe percentage of successful searches %f" , avg_success);
+void bubblesort()
+{
+    count=0;
+    int arr[MAX]={83,25,243,2,24,5,42,34};
+   int i, j;
+   
+   printf("\nArray before sorting:\n");
+    for( i = 0; i < MAX; i++)
+    {
+        printf("%d\t", arr[i]);
+    }
+    printf("\n\n\n");
+    
+   for (i = 0; i < MAX-1; i++)
+     count++;
+       // Last i elements are already in place
+       for (j = 0; j < MAX-i-1; j++)
+       count++;
+           if (arr[j] > arr[j+1])
+              swap(&arr[j], &arr[j+1]);
+              count++;
+              
+    display(arr);
+    printf("\n Time complexity : %d\n", count);
+}
 
-   return 0;
+
+void display(int arr[])
+{
+    int i;
+    for(i=0; i< MAX; i++)
+    {
+        printf("%d\t", arr[i]);
+    }
+    printf("\n==================================");
+}
+
+
+
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
 }
